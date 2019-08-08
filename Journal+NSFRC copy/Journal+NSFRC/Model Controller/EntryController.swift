eea7f7bfa8727520b5c28b9499cfb964d510d027
Kevin Tanner <kevindavidtanner@gmail.com>
2019-08-08 11:18:10 -0600

    adds comments before switching out the local source of truth

--- Journal+NSFRC copy/Journal+NSFRC/Model Controller/EntryController.swift
+++ Journal+NSFRC copy/Journal+NSFRC/Model Controller/EntryController.swift
@@ -13,13 +13,16 @@ class EntryController {
     
     static let sharedInstance = EntryController()
     
+    /// Entries is a computed Property. Its getting its values from the results of a NSFetchRequest. The <Model> means defines the generic type. This ensures that our entries array can ONLY hold Entry Objects.
     var entries: [Entry] {
         let fetchRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
         return (try? CoreDataStack.context.fetch(fetchRequest)) ?? []
     }
     
     //CRUD
+    /// We define a createEntry method that takes in two strings: Title, and body.
     func createEntry(withTitle: String, withBody: String) {
+        // Then we are using the convenience init we extended the Entry Class with and pass in those strings. This creates our Entry Objects with all required data.
         let _ = Entry(title: withTitle, body: withBody)
         
         saveToPersistentStore()
@@ -32,11 +35,13 @@ class EntryController {
         saveToPersistentStore()
     }
     
+    /// Removing Entry from persistent store
     func deleteEntry(entry: Entry) {
         entry.managedObjectContext?.delete(entry)
         saveToPersistentStore()
     }
     
+    /// We are 'attempting' to save all our Entry Data to our CoreDataStack(s) Persistent Store
     func saveToPersistentStore() {
         do {
              try CoreDataStack.context.save()
--- Journal+NSFRC copy/Journal+NSFRC/Model/Entry+Convenience.swift
+++ Journal+NSFRC copy/Journal+NSFRC/Model/Entry+Convenience.swift
@@ -10,12 +10,13 @@ import Foundation
 import CoreData
 
 extension Entry {
-    convenience init(title:String, body: String, timestamp: Date = Date(), context: NSManagedObjectContext = CoreDataStack.context) {
-        
+    /// Delcare our convenience init. We are adding a default value for our MOC and Date. We call the memberwise init of Entry and init with our context.
+    convenience init(title: String, body: String, timestamp: Date = Date(), context: NSManagedObjectContext = CoreDataStack.context) {
+        // For memberwise
         self.init(context: context)
+        // For convenience
         self.title = title
         self.body = body
         self.timestamp = timestamp
-        
     }
 }
